ba7b5eaecc72f872d63027b9d391bda453f6e6b2
BailerRuslan <ruslantolebay@gmail.com>
2024-04-03 12:23:20 +0500

    new model typeEngine added

---
+++ app/Http/Controllers/TypeEngineController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoretypeEngineRequest;
+use App\Http\Requests\UpdatetypeEngineRequest;
+use App\Models\typeEngine;
+
+class TypeEngineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoretypeEngineRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoretypeEngineRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\typeEngine  $typeEngine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(typeEngine $typeEngine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\typeEngine  $typeEngine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(typeEngine $typeEngine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatetypeEngineRequest  $request
+     * @param  \App\Models\typeEngine  $typeEngine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatetypeEngineRequest $request, typeEngine $typeEngine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\typeEngine  $typeEngine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(typeEngine $typeEngine)
+    {
+        //
+    }
+}
