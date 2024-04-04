6762aa229c719abc09d4542aafb7d693e1ce1603
BailerRuslan <ruslantolebay@gmail.com>
2024-04-04 22:18:34 +0500

    migraton type engine

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
      $this->call([
         CarSeeder::class,
+        TypeEngineSeeder::class,
         EngineSeeder::class,
         CitySeeder::class,
         ClientSeeder::class
