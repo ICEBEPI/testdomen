7f869363338490fdb622e531cfc4b042dbc5868f
BailerRuslan <ruslantolebay@gmail.com>
2024-04-04 22:19:10 +0500

    show client finished

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -1,42 +1,48 @@
 @extends('master')
 @section('content')
-    <h1 class="text-3xl font-bold mb-4 text-center text-gray-800">{{ $title ?? 'Данные автомобиля' }}</h1>
+    <h1 class="text-3xl font-bold mb-4 text-center text-gray-800">{{ $title ?? 'Данные клиента' }}</h1>
 
     <div class="bg-gray-100 rounded-lg shadow-lg p-4 mb-6">
-        <h2 class="text-2xl font-bold underline"><a href="{{ route('cars.edit', $car) }}">Редактировать</a></h2>
+        <h2 class="text-2xl font-bold underline"><a href="{{ route('clients.edit', $client) }}">Редактировать</a></h2>
     </div>
 
     <div class="bg-gray-100 rounded-lg shadow-lg p-4 mb-6">
-        <h1 class="text-xl font-semibold mb-2">Владелец:</h1>
-        <h2 class="text-2xl font-bold text-red-600 underline">{{ $car->client?->name ?? 'Нет владельца' }}</h2>
+        <h1 class="text-xl font-semibold mb-2">ФИО:</h1>
+
+        <h2 class="text-2xl font-bold font-bold underline"> <a href="{{ route('clients.edit', $client) }}">{{ $client->name }}
+        </h2></a>
     </div>
 
     <div class="bg-gray-100 rounded-lg shadow-lg p-4 mb-6">
-        <h1 class="text-xl font-semibold mb-2">Номер авто:</h1>
-        <h2 class="text-2xl font-bold text-red-600 underline">{{ $car->number }}</h2>
+        <h1 class="text-xl font-semibold mb-2">Номер телефона:</h1>
+        <h2 class="text-2xl font-bold">{{ $client->phone }}</h2>
     </div>
 
     <div class="bg-gray-100 rounded-lg shadow-lg p-4 mb-6">
-        <h2 class="text-xl font-semibold mb-2">Год:</h2>
-        <h2 class="text-2xl font-bold">{{ $car->year }}</h2>
-
-        <h2 class="text-xl font-semibold mb-2 mt-4">Бренд:</h2>
-        <h2 class="text-2xl font-bold">{{ $car->brand }}</h2>
-
-        <h2 class="text-xl font-semibold mb-2 mt-4">Количество мест:</h2>
-        <h2 class="text-2xl font-bold">{{ $car->seats }}</h2>
+        <h1 class="text-xl font-semibold mb-2">Город:</h1>
+        <h2 class="text-2xl font-bold ">{{ $client->city->name }}</h2>
     </div>
-
-    <h1 class="text-3xl font-bold mb-4 text-center text-gray-800">Двигатель:</h1>
 
     <div class="bg-gray-100 rounded-lg shadow-lg p-4 mb-6">
-        <h2 class="text-xl font-semibold mb-2">Объем:</h2>
-        <h2 class="text-2xl font-bold">{{ $car->engine->volume }}</h2>
-
-        <h2 class="text-xl font-semibold mb-2 mt-4">Тип:</h2>
-        <h2 class="text-2xl font-bold">{{ $car->engine->type }}</h2>
-
-        <h2 class="text-xl font-semibold mb-2 mt-4">Мощность:</h2>
-        <h2 class="text-2xl font-bold">{{ $car->engine->hp }} л.с.</h2>
+        <h1 class="text-xl font-semibold mb-2">Год рождения:</h1>
+        <h2 class="text-2xl font-bold">{{ $client->birthday }}</h2>
     </div>
+
+    <div class="bg-gray-100 rounded-lg shadow-lg p-4 mb-6">
+        @if ($client->cars->count())
+            <h2 class="text-xl font-semibold mb-2 mt-4">Список авто</h2>
+            <ol class="text-2xl font-bold">
+                @foreach ($client->cars as $car)
+                    <li>
+                        <a href="{{ route('cars.show', $car) }}">
+                            {{ $car->brand }} ({{ $car->year }})
+                        </a>
+                    </li>
+                @endforeach
+            </ol>
+        @endif
+    </div>
+    @if ($client->cars->count() == 0)
+        <h2 class="text-xl font-semibold mb-2 mt-4">Нет авто</h2>
+    @endif
 @endsection
--- resources/views/engines/form.blade.php
+++ resources/views/engines/form.blade.php
@@ -36,9 +36,10 @@
             <div class="mt-2">
                 <select id="type" name="type"
                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                    <option @if (isset($engine) && $engine->type == 'benzine') selected @endif value="benzine"> Benzine </option>
-                    <option @if (isset($engine) && $engine->type == 'diezel') selected @endif value="diezel"> Diezel </option>
-                    <option @if (isset($engine) && $engine->type == 'electric') selected @endif value="electric"> Electric </option>
+                    @foreach ($types as $type)
+                        <option @if(isset($type) && $type->type_id == $type->id) selected @endif value="{{ $type->id }}">{{ $type->name }}
+                    </option>
+                @endforeach
                 </select>
             </div>
         </div>
