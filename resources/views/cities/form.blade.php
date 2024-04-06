37eac9426c6a734fb8cf4f75e3efa1571c43c367
ICEBEPI <americanovi4@mail.ru>
2024-04-06 14:38:21 +0500

    city create updated

--- resources/views/cities/form.blade.php
+++ resources/views/cities/form.blade.php
@@ -7,19 +7,20 @@
                 Добавить город
             @endif
         </h2>
-
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8">
-            <div class="sm:col-span-4">
+        @if (isset($city))
             <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Город</label>
-                <select id="name" name="name"
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                    @foreach ($cities as $city)
-                        <option @if(isset($city) && $city->city_id == $city->id) selected @endif    value="{{ $city->id }}">{{ $city->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
+            <select id="name" name="name"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                @foreach ($cities as $city)
+                    <option @if ($city->id == $city->id) selected @endif value="{{ $city->id }}">
+                        {{ $city->name }}</option>
+                @endforeach
+            </select>
+        @else
+            <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Новый город</label>
+            <input type="text" id="name" name="name" placeholder="Введите название города"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+        @endif
     </div>
 </div>
 
