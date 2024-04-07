caf5a73b6fb93e50b9389cdc5dabbf6a0be18dfc
BailerRuslan <ruslantolebay@gmail.com>
2024-04-07 15:52:29 +0500

    связи

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Car extends Model
 {
@@ -23,5 +25,10 @@ class Car extends Model
     public function brand()
     {
         return $this->belongsTo(Brand::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Orders::class);
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -13,5 +15,20 @@ class Order extends Model
     public function services(): BelongsToMany
     {
         return $this->belongsToMany(Service::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
     }
 }
